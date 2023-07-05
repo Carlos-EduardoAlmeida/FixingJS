1b0b9e2b8d29b6a465d37dd6787c7b99aeeb7f27
carlos_eduardo <ceduardoeab10@gmail.com>
2023-07-05 09:38:28 -0300

    ajustes

--- ex017Tabuada/script.js
+++ ex017Tabuada/script.js
@@ -3,12 +3,12 @@ function executar(){
     var tabela = document.querySelector("select#tabela")
 
     var n = Number(num.value)
-    c = 1
+    let c = 1
 
     tabela.innerHTML = ""
     while(c<=10){
         let item = document.createElement("option")
-        mult = n * c
+        let mult = n * c
         item.text = `${n} x ${c} = ${mult}`
         tabela.appendChild(item)
         //tabela.innerHTML += `<td>${n} x ${c} = ${mult}</td>`
